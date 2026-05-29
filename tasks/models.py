1419bcc8bfe3151d5797cbb15031753695b57f42
G1olog1c <bomshgun@gmail.com>
2026-05-29 03:57:01 +0200

    Add priority, due date and list filter

--- tasks/models.py
+++ tasks/models.py
@@ -1,15 +1,28 @@
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
 class Task(models.Model):
     """Single to-do item belonging to a user."""
 
+    class Priority(models.TextChoices):
+        LOW = 'L', _('Niski')
+        MEDIUM = 'M', _('Średni')
+        HIGH = 'H', _('Wysoki')
+
     title = models.CharField(_('Tytuł'), max_length=200)
     description = models.TextField(_('Opis'), blank=True)
     is_done = models.BooleanField(_('Wykonane'), default=False)
+    priority = models.CharField(
+        _('Priorytet'),
+        max_length=1,
+        choices=Priority.choices,
+        default=Priority.MEDIUM,
+    )
+    due_date = models.DateField(_('Termin'), null=True, blank=True)
     created_at = models.DateTimeField(_('Utworzono'), auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
@@ -28,3 +41,10 @@ class Task(models.Model):
 
     def get_absolute_url(self):
         return reverse('task_list')
+
+    @property
+    def is_overdue(self):
+        """Return True if the task is past its due date and not completed."""
+        if self.due_date and not self.is_done:
+            return self.due_date < timezone.localdate()
+        return False
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Case, IntegerField, Value, When
 from django.views.generic import (
     CreateView,
     DeleteView,
@@ -13,19 +14,41 @@ from .models import Task
 
 
 class TaskListView(LoginRequiredMixin, ListView):
-    """Display tasks belonging to the current user."""
+    """Display tasks belonging to the current user, with optional filter."""
 
     model = Task
     template_name = 'tasks/task_list.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
-        return Task.objects.filter(user=self.request.user)
+        # Order priorities explicitly: High -> Medium -> Low
+        priority_order = Case(
+            When(priority='H', then=Value(0)),
+            When(priority='M', then=Value(1)),
+            When(priority='L', then=Value(2)),
+            output_field=IntegerField(),
+        )
+        qs = Task.objects.filter(user=self.request.user).annotate(
+            priority_rank=priority_order
+        ).order_by('is_done', 'priority_rank', 'due_date', '-created_at')
+
+        # Filter from query string: ?filter=active or ?filter=done
+        filter_value = self.request.GET.get('filter')
+        if filter_value == 'active':
+            qs = qs.filter(is_done=False)
+        elif filter_value == 'done':
+            qs = qs.filter(is_done=True)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['current_filter'] = self.request.GET.get('filter', 'all')
+        return context
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
     model = Task
-    fields = ['title', 'description']
+    fields = ['title', 'description', 'priority', 'due_date']
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task_list')
 
@@ -37,14 +60,12 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
 
 class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = ['title', 'description', 'is_done']
+    fields = ['title', 'description', 'priority', 'due_date', 'is_done']
     template_name = 'tasks/task_form.html'
     success_url = reverse_lazy('task_list')
 
     def get_queryset(self):
-        # Prevent editing other users' tasks
         return Task.objects.filter(user=self.request.user)
-
 
 class TaskDeleteView(LoginRequiredMixin, DeleteView):
     model = Task
